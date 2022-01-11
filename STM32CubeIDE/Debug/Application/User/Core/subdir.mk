6f6f2b142e28fd917bdfc94e7ca4ae120727c831
iplion <iplion@mail.ru>
2022-01-11 15:51:26 +0700

    first commit

---
+++ STM32CubeIDE/Debug/Application/User/Core/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+/home/grib123/cube/2.3_10_light_server/Core/Src/main.c \
+/home/grib123/cube/2.3_10_light_server/Core/Src/stm32f1xx_hal_msp.c \
+/home/grib123/cube/2.3_10_light_server/Core/Src/stm32f1xx_it.c \
+../Application/User/Core/syscalls.c \
+../Application/User/Core/sysmem.c 
+
+OBJS += \
+./Application/User/Core/main.o \
+./Application/User/Core/stm32f1xx_hal_msp.o \
+./Application/User/Core/stm32f1xx_it.o \
+./Application/User/Core/syscalls.o \
+./Application/User/Core/sysmem.o 
+
+C_DEPS += \
+./Application/User/Core/main.d \
+./Application/User/Core/stm32f1xx_hal_msp.d \
+./Application/User/Core/stm32f1xx_it.d \
+./Application/User/Core/syscalls.d \
+./Application/User/Core/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Application/User/Core/main.o: /home/grib123/cube/2.3_10_light_server/Core/Src/main.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../../Core/Inc -I../../Drivers/STM32F1xx_HAL_Driver/Inc -I../../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Application/User/Core/stm32f1xx_hal_msp.o: /home/grib123/cube/2.3_10_light_server/Core/Src/stm32f1xx_hal_msp.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../../Core/Inc -I../../Drivers/STM32F1xx_HAL_Driver/Inc -I../../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Application/User/Core/stm32f1xx_it.o: /home/grib123/cube/2.3_10_light_server/Core/Src/stm32f1xx_it.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../../Core/Inc -I../../Drivers/STM32F1xx_HAL_Driver/Inc -I../../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Application/User/Core/%.o: ../Application/User/Core/%.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../../Core/Inc -I../../Drivers/STM32F1xx_HAL_Driver/Inc -I../../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
